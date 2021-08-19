1b4197ce4df68cddb756183cc4e07be9e351a634
gerry <guorui.developer@gmail.com>
2021-08-19 00:07:21 -0700

    XPermission lib & Demo.

--- app/src/main/java/com/xpermission/app/MainActivity.kt
+++ app/src/main/java/com/xpermission/app/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.xpermission.app
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.xpermission.xpm.XPermission
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            XPermission.request(this, Manifest.permission.CALL_PHONE) { allGranted, deniedList ->
+                if (allGranted) {
+                    call()
+                } else {
+                    Toast.makeText(this, "You denied $deniedList", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun call() {
+        startActivity(Intent(Intent.ACTION_CALL, Uri.parse("tel:4088210000")))
     }
 }
